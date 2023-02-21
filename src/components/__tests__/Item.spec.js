8c60fd45300e930e0e1e2e4760ab5c2429538251
Dave Mackey <dave@davemackey.net>
2023-02-21 08:43:35 -0500

    Create test w/out Vue Test Utils

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,8 +1,16 @@
 import Item from '../Item.vue'
 import Vue from 'vue'
 
-describe('Item', () => {
-  test('sanity test', () => {
-    console.log(Item)
+// Without using Vue Test Utils, involves boilerplate
+
+describe('Item.vue', () => {
+  test('renders "item"', () => {
+    // Ctor = constructor
+    const Ctor = Vue.extend(Item) // Create a Vue constructor from the options
+    // Create an instance, convention is to refer to Vue instance as vm
+    // Explicitly call $mount method because we don't have an el option in constructor so it doesn't auto mount
+    const vm = new Ctor().$mount()
+    // Vue generates DOM nodes, accessible with $el property
+    expect(vm.$el.textContent).toContain('item')
   })
 })
