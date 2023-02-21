e4ab4318f54d6ff5c10af9d4006bc9d2edab5142
Dave Mackey <dave@davemackey.net>
2023-02-21 09:45:19 -0500

    Check that rendered items have expected props

--- src/views/__tests__/ItemList.spec.js
+++ src/views/__tests__/ItemList.spec.js
@@ -3,11 +3,13 @@ import ItemList from '../ItemList.vue'
 import Item from '../../components/Item.vue'
 
 describe('ItemList.vue', () => {
-  test('renders an Item for each item in window.items', () => {
+  test('renders an Item with data for each item in window.items', () => {
     window.items = [{}, {}, {}] // Sets items data for component to use
     const wrapper = shallowMount(ItemList)
-    // Use a WrapperArray length property to check an Item is rendered for each item in window.items.
-    expect(wrapper.findAll(Item))
-      .toHaveLength(window.items.length)
+    const items = wrapper.findAll(Item) // Creates WrapperArray of Item components
+    expect(items).toHaveLength(window.items.length)
+    items.wrappers.forEach((wrapper, i) => {
+      expect(wrapper.props().item).toBe(window.items[i])
+    })
   })
 })
