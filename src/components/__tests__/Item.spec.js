761dea8f411122f0f037c546ba5d75e328517043
Dave Mackey <dave@davemackey.net>
2023-02-21 08:48:19 -0500

    First vue test utils component and install depends

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,16 +1,13 @@
+// No longer need to import Vue, since using Vue Test Utils
+import { mount } from '@vue/test-utils'
 import Item from '../Item.vue'
-import Vue from 'vue'
-
-// Without using Vue Test Utils, involves boilerplate
 
 describe('Item.vue', () => {
   test('renders "item"', () => {
-    // Ctor = constructor
-    const Ctor = Vue.extend(Item) // Create a Vue constructor from the options
-    // Create an instance, convention is to refer to Vue instance as vm
-    // Explicitly call $mount method because we don't have an el option in constructor so it doesn't auto mount
-    const vm = new Ctor().$mount()
-    // Vue generates DOM nodes, accessible with $el property
-    expect(vm.$el.textContent).toContain('item')
+    const wrapper = mount(Item) // Returns wrapper containing mounted Item
+    expect(wrapper.text()).toContain('item') // Returns item text content, text is a helper method
   })
 })
+
+// Wrapper includes the Vue instance (vm) but also additional helper methods - e.g. to set props,
+// check instance properties, perform actions on instance.
