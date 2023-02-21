667af309b30068b4c9d059f6ad63d1d5e2026dad
Dave Mackey <dave@davemackey.net>
2023-02-21 08:51:14 -0500

    Add shallowMount test

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,11 +1,15 @@
 // No longer need to import Vue, since using Vue Test Utils
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import Item from '../Item.vue'
 
 describe('Item.vue', () => {
   test('renders "item"', () => {
     const wrapper = mount(Item) // Returns wrapper containing mounted Item
     expect(wrapper.text()).toContain('item') // Returns item text content, text is a helper method
+  })
+  test('renders "item" using shallowMount', () => {
+    const wrapper = shallowMount(Item) // Stubs any children components and mounts Item, allows testing component in isolation
+    expect(wrapper.text()).toContain('item')
   })
 })
 
